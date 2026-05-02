9189208ef3838a36bda643035ba9be35079902bd
Daniel Aparisi <daniel.aparisi.lozano@gmail.com>
2026-05-02 20:05:43 +0200

    Starting interfazUusario

--- src/main/java/InterfazUsuario.java
+++ src/main/java/InterfazUsuario.java
@@ -6,6 +6,8 @@ public class InterfazUsuario {
 
     public InterfazUsuario(int maxIngredientes, int maxInstrucciones, int maxRecetasEnLibro) {
         // @todo
+        
+
     }
 
     public InterfazUsuario(int maxIngredientes, int maxInstrucciones, int maxRecetasEnLibro, String archivoRecetas) {
@@ -17,21 +19,59 @@ public class InterfazUsuario {
     }
 
     private void menuPrincipal(Scanner scanner) {
-        // @todo
+            String cadena = "";
+            cadena += "--- Menú Principal ---\n";
+            cadena += "1. Agregar Receta\n";
+            cadena += "2. Consultar/Editar Receta\n";
+            cadena += "3. Planificar Comidas\n";
+            cadena += "4. Guardar Recetas\n";
+            cadena += "5. Cargar Recetas\n";
+            cadena += "6. Guardar Plan Semanal\n";
+            cadena += "7. Salir\n\n";
+
+            cadena += "Elige una opción: ";
+
+            int opcion;
+            do{
+                System.out.println();
+                opcion = Utilidades.leerNumero(scanner, cadena, 1, 7);
+                switch (opcion) {
+                    case 1 -> agregarReceta(scanner);
+                    case 2 -> consultarReceta(scanner);
+                    case 3 -> planificarComidas(scanner);
+                    case 4 -> guardarRecetas(scanner);
+                    case 5 -> cargarRecetas(scanner);
+                    case 6 -> guardarPlanSemanal(scanner);
+                }
+            } while (opcion != 7);
+            System.out.println("¡Has salido del recetario con éxito!");
+        }
     }
 
     private void agregarReceta(Scanner scanner) {
         // @todo
+        /* El método debe solicitar al usua-
+        rio el nombre de la receta. A continuación, debe solicitar al
+        usuario que introduzca los ingredientes e instrucciones de la
+        receta. Una vez introducidos los datos, el método debe crear
+        un objeto de la clase Receta con la información proporciona-
+        da y añadir la receta al libro de recetas. Si la receta se añade
+        correctamente, se debe mostrar un mensaje de éxito */
+        if(LibroDeRecetas.rece)
+
+
+        System.out.println("Nombre de la recets: ");
+
     }
 
     private void consultarReceta(Scanner scanner) {
         // @todo
     }
 
-    //private Receta buscarRecetaPorNombre(Scanner scanner) {
+    private Receta buscarRecetaPorNombre(Scanner scanner) {
         // @todo
 
-    //}
+    }
 
     private void editarReceta(Scanner scanner, Receta seleccionada) {
         // @todo
@@ -60,4 +100,4 @@ public class InterfazUsuario {
     private void mostrarError(String mensaje) {
         // @todo
     }
-}
+
