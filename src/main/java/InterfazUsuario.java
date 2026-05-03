437062ae8e08e7bffcfb5b401019b714771d1873
Daniel Aparisi <daniel.aparisi.lozano@gmail.com>
2026-05-03 13:49:59 +0200

    working on InterfazUsuario

--- src/main/java/InterfazUsuario.java
+++ src/main/java/InterfazUsuario.java
@@ -18,6 +18,8 @@ public class InterfazUsuario {
 
         public InterfazUsuario(int maxIngredientes, int maxInstrucciones, int maxRecetasEnLibro, String archivoRecetas) {
             // @todo
+            this.maxIngredientes = maxIngredientes;
+            this.maxInstrucciones = maxInstrucciones;
         }
 
         public void iniciar() {
@@ -71,15 +73,42 @@ public class InterfazUsuario {
 
             if(!libroRecetas.recetasCompletas()){
 
-            String nombre;
-            nombre = Utilidades.leerCadena(scanner,"nombre de la cadena ");
-            //aqui crea el una receta nueva con ese nombre
-            Receta receta = new Receta(nombre, maxIngredientes, maxInstrucciones);
+                String nombre;
+                nombre = Utilidades.leerCadena(scanner,"nombre de la cadena ");
+                //aqui crea el una receta nueva con ese nombre
+                Receta receta = new Receta(nombre, maxIngredientes, maxInstrucciones);
+                boolean fin = false;
 
-            String ingrediente ;
-            ingrediente = Utilidades.leerCadena(scanner,"Introduce los ingredientes (una línea por ingrediente, escribe 'fin' para terminar");
+                String ingrediente ;
+                ingrediente = Utilidades.leerCadena(scanner,"Introduce los ingredientes (una línea por ingrediente, escribe 'fin' para terminar");
+                //mientras no lea fin o FIN .equalsIgnoraCase()
+                while(!ingrediente.equalsIgnoreCase("fin") && !fin){
+                    receta.agregarIngrediente(ingrediente);
+
+                    if(!receta.ingredientesCompletos()){
+                        ingrediente = Utilidades.leerCadena(scanner,"Introduce los ingredientes (una línea por ingrediente, escribe 'fin' para terminar");
+                    } else{
+                        System.out.println("Ha añadido el número máximo de ingredientes posible.");
+                        fin = true;
+                    }
+                }
 
 
+                fin = false;
+                String instruccion;
+                instruccion = Utilidades.leerCadena(scanner, "Introduce las instrucciones (una línea por instrucción, escribe 'fin' para terminar): ");
+                while (!instruccion.equalsIgnoreCase("fin") && !fin) {
+                    receta.agregarInstruccion(instruccion);
+                    if (!receta.instruccionesCompletas()) {
+                        instruccion = Utilidades.leerCadena(scanner, "Introduce las instrucciones (una línea por instrucción, escribe 'fin' para terminar): ");
+                    } else {
+                        System.out.println("Ha añadido el número máximo de instrucciones posible.");
+                        fin = true;
+                    }
+                }
+
+                libroRecetas.agregarReceta(receta);
+                System.out.println("!Receta agregada exitosamente");
             }else{
                 System.out.println("No se pudo añadir la receta");
             }
@@ -87,11 +116,37 @@ public class InterfazUsuario {
         }
 
         private void consultarReceta(Scanner scanner) {
-            // @todo
+            Receta busqueda = buscarRecetaPorNombre(scanner);
+            if(!busqueda.getNombre().equalsIgnoreCase("ELIMINAR")){
+                System.out.println(busqueda);
+                System.out.println();
+                editarReceta(scanner, busqueda);
+            }
+
         }
 
         private Receta buscarRecetaPorNombre(Scanner scanner) {
-            // @todo
+            String textoBusqueda = Utilidades.leerCadena(scanner,"Introduce el texto de la receta a buscar (-FIN- para volver):" );
+            Receta recetaEncontrada;
+            boolean fin = false;
+            while (textoBusqueda.equalsIgnoreCase("fin") || textoBusqueda.equalsIgnoreCase("-FIN-")){
+                Receta[] recetasEncontradas = libroRecetas.buscarRecetaPorNombre(textoBusqueda);
+                //verificamos si el array esta vacio
+                while (recetasEncontradas[0] == null && !textoBusqueda.equalsIgnoreCase("fin") && !textoBusqueda.equalsIgnoreCase("-FIN-")){
+                    System.out.println("No se han encontrado recetas con ese nombre. Prueba otra vez.");
+                    textoBusqueda = Utilidades.leerCadena(scanner, "Introduce el texto de la receta a buscar (-FIN- para volver): ");
+                    recetasEncontradas = libroRecetas.buscarRecetaPorNombre(textoBusqueda);
+                }
+
+                //si no esta vacio mostramos las recetas
+              if (!textoBusqueda.equalsIgnoreCase("fin") && !textoBusqueda.equalsIgnoreCase("-FIN-")){
+
+                  System.out.println("Recetas encontradas");
+                  recetaEncontrada = seleccionarReceta(scanner, recetasEncontradas);
+              } else{
+                  recetaEncontrada = new Receta("ELIMINAR", maxIngredientes, maxInstrucciones);
+              }
+            }
             return null;
         }
 
@@ -99,9 +154,10 @@ public class InterfazUsuario {
             // @todo
         }
 
-        //private Receta seleccionarReceta(Scanner scanner, Receta[] recetas) {
+        private Receta seleccionarReceta(Scanner scanner, Receta[] recetas) {
             // @todo
-        //}
+            return null;
+        }
 
         private void planificarComidas(Scanner scanner) {
             // @todo
